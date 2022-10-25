7b6c50ac7366552e11b3f6c9255c528164156e22
akshay722 <akshayvarmamit@gmail.com>
2022-10-25 14:20:06 +0530

    fixes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,13 @@
-import { slide as Menu } from "react-burger-menu";
+import { slide as SideMenu } from "react-burger-menu";
 
-export default (props) => {
+export const Menu = (props) => {
   let { ref, expRef, skillsRef } = props;
   const handleClick = (ref) => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   };
   return (
     // Pass on our props
-    <Menu {...props}>
+    <SideMenu {...props}>
       <ul className="sm:flex gap-6 ml-auto px-10 items-center hidden">
         <li className="cursor-pointer" onClick={() => handleClick(ref)}>
           About
@@ -19,6 +19,6 @@ export default (props) => {
           Skills
         </li>
       </ul>
-    </Menu>
+    </SideMenu>
   );
 };
